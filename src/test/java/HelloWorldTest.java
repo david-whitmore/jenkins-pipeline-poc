455fa02f48949e4463ab58ce125ae3172cc8b564
David Whitmore <whitters1793@googlemail.com>
2017-11-19 16:55:21 +0000

    master, intentionally write a failing test to check what happens.

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,8 +1,11 @@
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
 public class HelloWorldTest {
 	@Test
 	public void sayHello_doesNotThrowException() throws Exception {
 		new HelloWorld().sayHello();
+		fail();
 	}
 }
