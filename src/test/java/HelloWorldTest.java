7efc8a326c8b64723cd6695d4add9ccb18f61aaf
David Whitmore <whitters1793@googlemail.com>
2017-11-19 16:56:26 +0000

    master, fix failing test.

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,11 +1,8 @@
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 
 public class HelloWorldTest {
 	@Test
 	public void sayHello_doesNotThrowException() throws Exception {
 		new HelloWorld().sayHello();
-		fail();
 	}
 }
